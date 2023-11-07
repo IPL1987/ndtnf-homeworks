a49a5cc5393dba14287385ca3babe75f59d9da1a
IPL1987 <ihorpoznyak@yandex.com>
2023-11-07 20:08:27 +0300

    init commit

--- 009-rxjs/src/index.ts
+++ 009-rxjs/src/index.ts
@@ -1,6 +1,13 @@
-import {of, from, timer, range} from 'rxjs';
+import { forkJoin } from 'rxjs';
+import { ajax } from "rxjs/ajax";
+import { BITBUCKET, GITHUB } from "../config";
 
-const o = range(0, 10)
+const request = (url: string) => ajax.getJSON(url);
+const o =
+  forkJoin({
+    github: request(GITHUB),
+    bitbucket: request(BITBUCKET),
+  });
 
 o.subscribe({
   next: (value: any) => console.log('Next:', value),
